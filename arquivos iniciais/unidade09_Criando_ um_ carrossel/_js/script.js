7972767c222b5bf9bb4312cab7a9150e82617d2f
jmarcos9 <jmarcos9@gmail.com>
2021-03-05 21:44:17 -0300

    add news files completed course

---
+++ arquivos iniciais/unidade09_Criando_ um_ carrossel/_js/script.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.carousel').carousel({
+        interval:8000,
+       /* pause:"null" *//*pausa a transição se colocar o mouse em cima da foto */
+       /*wrap: false*/ /* quando chegar na ultima foto não volta automaticamente para a primeira */
+    });
+});
